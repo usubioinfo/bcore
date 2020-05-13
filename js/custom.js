9995af2075cb233fffd5a236c7c37523db2a6159
MusicDev33 <smccowan33@gmail.com>
2020-05-13 14:25:59 -0600

    Remove useless script

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-$( 'document' ).ready( function () {
-	var S = window.location.pathname;
-    S = S.substring(1, S.length);
-    console.log(S);
-    $('a[href^="' + S + '"]').parent().addClass('active');
-
-});
-
-
