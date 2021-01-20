05791e84ca8d7f951d65e6dbbc614c392f7b2a5d
MusicDev33 <smccowan33@gmail.com>
2021-01-20 14:57:38 -0700

    Show selected card

--- js/kbl/news.js
+++ js/kbl/news.js
@@ -4,8 +4,17 @@
     if (!nodeEl.classList.contains('d-none')) {
       nodeEl.classList.add('d-none');
     }
+    const phpEl = document.querySelector('.php7');
+    phpEl.classList.remove('d-none');
 
-    document.querySelector('.php7').classList.remove('d-none');
+    const phpCard = document.querySelector('.php-card');
+    const nodeCard = document.querySelector('.node-card');
+
+    if (!phpCard.classList.contains('selected')) {
+      phpCard.classList.add('selected');
+    }
+
+    nodeCard.classList.remove('selected');
   });
 
   document.querySelector('.node-card').addEventListener('click', e => {
@@ -15,5 +24,14 @@
     }
 
     document.querySelector('.node').classList.remove('d-none');
+
+    const nodeCard = document.querySelector('.node-card');
+    const phpCard = document.querySelector('.php-card');
+
+    if (!nodeCard.classList.contains('selected')) {
+      nodeCard.classList.add('selected');
+    }
+
+    phpCard.classList.remove('selected');
   });
 })();
