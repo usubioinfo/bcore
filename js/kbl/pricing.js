2ac52c14f43289124e5b60908804c1a1a05d7a17
MusicDev33 <smccowan33@gmail.com>
2021-07-26 13:06:50 -0600

    Add pricing

--- js/kbl/pricing.js
+++ js/kbl/pricing.js
@@ -1,28 +1,52 @@
 (function() {
-    'use strict';
-  
-    const switches = document.querySelectorAll('.kbl-switch');
-    switches[0].classList.add('current');
-  
-    let currentPage = switches[0].textContent.trim().toLowerCase();
-    console.log(currentPage);
-  
-    switches.forEach((el) => {
-      el.addEventListener('click', (e) => {
-        document.querySelectorAll('.kbl-switch').forEach((newEl) => {
-          newEl.classList.remove('current');
-        });
-  
-        el.classList.add('current');
-        currentPage = el.textContent.trim().toLowerCase();
-  
-        document.querySelectorAll('.pricing-section').forEach((newEl) => {
-          newEl.classList.remove('d-none');
-          newEl.classList.add('d-none');
-        });
-    
-        document.getElementById(currentPage).classList.remove('d-none');
+  'use strict';
+
+  const pricing = {
+    rnaseq: 400,
+    mirna: 500,
+    snpseq: 600,
+    chip: 600,
+    methyl: 700,
+    genome: 50
+  }
+
+  const switches = document.querySelectorAll('.kbl-switch');
+  switches[0].classList.add('current');
+
+  let currentPage = switches[0].textContent.trim().toLowerCase();
+  console.log(currentPage);
+
+  switches.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      document.querySelectorAll('.kbl-switch').forEach((newEl) => {
+        newEl.classList.remove('current');
       });
+
+      el.classList.add('current');
+      currentPage = el.textContent.trim().toLowerCase();
+
+      document.querySelectorAll('.pricing-section').forEach((newEl) => {
+        newEl.classList.remove('d-none');
+        newEl.classList.add('d-none');
+      });
+  
+      document.getElementById(currentPage).classList.remove('d-none');
     });
-  })();
+  });
+
+  document.querySelectorAll('.kbl-pricing-item').forEach(el => {
+    el.addEventListener('click', e => {
+      document.querySelectorAll('.kbl-pricing-item').forEach(newEl => {
+        newEl.classList.remove('selected')
+      });
+
+      el.classList.add('selected');
+
+      const pricingId = el.id;
+      const price = pricing[pricingId];
+      const priceEl = document.querySelector('#price');
+      priceEl.textContent = price;
+    });
+  });
+})();
   
