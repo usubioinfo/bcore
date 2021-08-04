27e164e056de7520d5714f56caba96e7ad8ace70
MusicDev33 <smccowan33@gmail.com>
2021-08-04 13:42:33 -0600

    Complete rewrite of searchbar

--- js/kbl/search.js
+++ js/kbl/search.js
@@ -1,3 +1,48 @@
 (() => {
-  
+  const pages = [
+    {
+      name: 'Home',
+      url: '/'
+    },
+    {
+      name: 'Services',
+      url: '/services'
+    },
+    {
+      name: 'News',
+      url: '/news'
+    },
+    {
+      name: 'Hardware',
+      url: '/hardware'
+    },
+    {
+      name: 'Software',
+      url: '/software'
+    },
+    {
+      name: 'Manuals',
+      url: '/manuals'
+    },
+    {
+      name: 'FAQ',
+      url: '/faq'
+    },
+    {
+      name: 'Pricing',
+      url: '/pricing'
+    }
+  ];
+
+  const searchPage = (searchTerm) => {
+    const results = pages.filter(item => {
+      return item.name.toLowerCase().includes(searchTerm);
+    });
+
+    return results
+  }
+
+  document.getElementById('nav-searchbar').addEventListener('onkeyup', e => {
+    console.log(searchPage(document.getElementById('nav-searchbar').value));
+  });
 })();
