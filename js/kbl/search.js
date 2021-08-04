afda44139d057966cb3ef62c25aa9acd7604915e
MusicDev33 <smccowan33@gmail.com>
2021-08-04 14:33:46 -0600

    Add search feature

--- js/kbl/search.js
+++ js/kbl/search.js
@@ -31,18 +31,99 @@
     {
       name: 'Pricing',
       url: '/pricing'
+    },
+    {
+      name: 'Contact',
+      url: '/contact'
+    },
+    {
+      name: 'RNA-Seq',
+      url: '/services/rnaseq'
+    },
+    {
+      name: 'VAR-Seq',
+      url: '/services/varseq'
+    },
+    {
+      name: 'Methyl-Seq',
+      url: '/services/methylation'
+    },
+    {
+      name: 'miRNA-Seq',
+      url: '/services/mirna'
+    },
+    {
+      name: 'ChIP-Seq',
+      url: '/services/chipseq'
+    },
+    {
+      name: 'GBS',
+      url: '/services/gbs'
+    },
+    {
+      name: 'Amplicon Analysis',
+      url: '/services/amplicon'
+    },
+    {
+      name: 'Metagenomics',
+      url: '/services/metagenomics'
+    },
+    {
+      name: 'Eukaryotes Assembly',
+      url: '/services/eukassembly'
+    },
+    {
+      name: 'Prokaryotes Assembly',
+      url: '/services/prokassembly'
     }
   ];
 
   const searchPage = (searchTerm) => {
     const results = pages.filter(item => {
-      return item.name.toLowerCase().includes(searchTerm);
+      return item.name.toLowerCase().startsWith(searchTerm);
     });
 
     return results
   }
 
-  document.getElementById('nav-searchbar').addEventListener('onkeyup', e => {
-    console.log(searchPage(document.getElementById('nav-searchbar').value));
+  const createNoRes = () => {
+    const newAnchor = document.createElement('a');
+    const content = document.createTextNode('No Results');
+
+    newAnchor.appendChild(content);
+    newAnchor.href = '#';
+
+    newAnchor.classList.add('dropdown-item');
+    newAnchor.classList.add('no-res');
+    newAnchor.setAttribute('id', 'no-res');
+
+    return newAnchor;
+  }
+
+  document.getElementById('nav-searchbar').addEventListener('keyup', e => {
+    const searchTerm = document.getElementById('nav-searchbar').value;
+
+    if (searchTerm.length === 0) {
+      document.getElementById('nav-search-dropdown').innerText = '';
+      document.getElementById('nav-search-dropdown').appendChild(createNoRes());
+      return;
+    }
+
+    const searchResults = searchPage(searchTerm);
+
+    document.getElementById('nav-search-dropdown').innerText = '';
+
+    for (let result of searchResults) {
+      const newAnchor = document.createElement('a');
+      const content = document.createTextNode(result.name);
+
+      newAnchor.appendChild(content);
+      newAnchor.setAttribute('href', result.url);
+
+      newAnchor.classList.add('dropdown-item');
+      newAnchor.classList.add('result');
+
+      document.getElementById('nav-search-dropdown').appendChild(newAnchor);
+    }
   });
 })();
