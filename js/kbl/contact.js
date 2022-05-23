69c74fc190a26658deb0c0cfee344352bebcb79f
papaSneetch <a02308059@usu.edu>
2022-05-23 12:36:35 -0600

    Changing email to Rakesh's email.

--- js/kbl/contact.js
+++ js/kbl/contact.js
@@ -9,7 +9,7 @@
     const body = {
       password: 'rkU56a%e$',
       subjectLine: `BioinfoCore Contact Request from ${name}`,
-      recipient: 'a02308059@usu.edu',
+      recipient: 'rkaundal@usu.edu',
       messageBody: messageBody
     }
 
