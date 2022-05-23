fbe0b34ad46e537b9fdbdfff96770d9a0a5b68e8
papaSneetch <a02308059@usu.edu>
2022-05-23 11:47:17 -0600

    Testing email change as it doesn't work with an invalid origin.

--- js/kbl/contact.js
+++ js/kbl/contact.js
@@ -1,17 +1,15 @@
 (() => {
   document.getElementById('send').addEventListener('click', e => {
-
     const name = document.getElementById('formName').value;
     const email = document.getElementById('formEmail').value;
     const phone = document.getElementById('formPhone').value;
     const message = document.getElementById('formMessage').value;
-
     const messageBody = message + '\n\n' + 'Contact me back at ' + email + ' or ' + phone;
 
     const body = {
       password: 'rkU56a%e$',
       subjectLine: `BioinfoCore Contact Request from ${name}`,
-      recipient: 'itsme@shelbymccowan.com',
+      recipient: 'a02308059@usu.edu',
       messageBody: messageBody
     }
 
